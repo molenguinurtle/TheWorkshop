efd5496959d29457c95b4705ad1a051c66bc2f73
MRYOUNG-PC\molenguinurtle <young.anthonyd@gmail.com>
2020-04-12 19:27:14 -0500

    GenerateSprites works. Can now cut up pictures into given dimensions. AssignSprites next.

--- Assets/Scripts1/CropUtilScripts/UIController.cs
+++ Assets/Scripts1/CropUtilScripts/UIController.cs
@@ -16,7 +16,13 @@ public class UIController : MonoBehaviour {
 
     public Image mySprite;
     public Sprite[] currentSprites;
+    public Image[] currentImages;
     private List<Sprite> tempSpriteLst;
+
+    public int LANDSCAPE_WIDTH_CONST;
+    public int LANDSCAPE_HEIGHT_CONST;
+    public int PORTRAIT_WIDTH_CONST;
+    public int PORTRAIT_HEIGHT_CONST;
 
     private void Start()
     {
@@ -28,36 +34,75 @@ public class UIController : MonoBehaviour {
             GenerateSprites();
     }
 
+    public void AssignSpritesPressed()
+    {
+        if (currentSprites.Length > 11)
+        {
+            AssignSprites();
+        }
+    }
     private void GenerateSprites()
     {
+        //First, get our Picture
         Texture2D texToCut = mySprite.sprite.texture;
-        //int x = Mathf.FloorToInt(sourceRect.x);
-        //int y = Mathf.FloorToInt(sourceRect.y);
-        int x = Mathf.FloorToInt(0); //These (x,y of the 'rect' for cuttin up Pictures) will change as part of a for loop (hopefully not too complicated) or 3
-        //Something like below. 'Mathf.FloorToInt(texToCut.width/4f)' will actually just be the 'widthConstant' discussed below. The same will go for 'y' and the 'heightConstant' 
-        //for (x =0; x < texToCut.width; x += Mathf.FloorToInt(texToCut.width/4f))
-        int y = Mathf.FloorToInt(0);
-        int width = Mathf.FloorToInt(1024);//This will need to be a figured out constant #; Will change depending on if an image is Landscape or Portrait style; Once a resolution
-                                            // is decided upon for the photos and the "play area"(read: place where user clicks image pieces to rotate them), this will be set. Might change
-                                            //depending on System specs, operating system, etc. (FUUUUUUUUUUU)
-        //int height = Mathf.FloorToInt(768);//Same as above
-        int height = Mathf.FloorToInt(1536);
-        Color[] pix = texToCut.GetPixels(x, y, width, height);
-        Texture2D destTex = new Texture2D(width, height);
-        destTex.SetPixels(pix);
-        destTex.Apply();
+        int width = 0;
+        int height = 0;
+        //Set w/h vars based on Landscape/Portrait. Can add in SQUARE Constants later
+        if (texToCut.width > texToCut.height)
+        {
+            //Use Landscape Constants
+            width = LANDSCAPE_WIDTH_CONST;
+            height = LANDSCAPE_HEIGHT_CONST;
+        }
+        else
+        {
+            //Use Portrait Constants
+            width = PORTRAIT_WIDTH_CONST;
+            height = PORTRAIT_HEIGHT_CONST;
+        }
+        //Reset the tempSpriteLst
+        tempSpriteLst = new List<Sprite>();
 
-        // Set the object's texture to show the extracted rectangle.
-        mySprite.sprite = Sprite.Create(destTex,
+        //Cut the picture up into width x height sized sprites
+        for (int sawY = 0; sawY < texToCut.height;sawY+= height)
+        {
+            for (int sawX =0; sawX < texToCut.width; sawX += width)
+            {
+                Color[] pix = texToCut.GetPixels(sawX, sawY, width, height);
+                Texture2D cutTex = new Texture2D(width, height);
+                cutTex.SetPixels(pix);
+                cutTex.Apply();
+                tempSpriteLst.Add(Sprite.Create(cutTex,
                                 new Rect(0, 0,
-                                          destTex.width,
-                                          destTex.height),
-                                                new Vector2(0, 0), 100.0f);
-        mySprite.SetNativeSize();
-        //C'est la vie. OK OK OK. We got it! See notes above. Basically, GetPixels is referencing the actual texture/PICTURE in the Assets folder. So, the 'rect' that we make to cut up
-        // the Photos must reference their dimensions, NOT the dimensions of 'mySprite.' AKA, whatever you have your IMAGE_HOLDER's Width and Height set to DOES. NOT. MATTER. for GetPixels.
+                                          cutTex.width,
+                                          cutTex.height),
+                                                new Vector2(0, 0), 100.0f));
+            }
+        }
+
+        //Set the currentSprites to those Sprites
+        currentSprites = tempSpriteLst.ToArray();
+
+
     }
 
+    //C'est la vie. We're cutting up the Pictures into sprites. Now we just need a collection of Buttons/Images(prolly just Images) that are set up to swap out Sprites painlessly
+    // AKA, AssignSprites will basically be a for loop where we loop thru currentSprites and assign to an Image[x]'s sprite component. See commented out code below
+    private void AssignSprites()
+    {
+        for (int x = 0; x < currentSprites.Length; x++)
+        {
+            currentImages[x].sprite = currentSprites[x];
+            currentImages[x].SetNativeSize();
+        }
+        // Set the object's texture to show the extracted rectangle.
+        //mySprite.sprite = Sprite.Create(destTex,
+        //                        new Rect(0, 0,
+        //                                  destTex.width,
+        //                                  destTex.height),
+        //                                        new Vector2(0, 0), 100.0f);
+        //mySprite.SetNativeSize();
+    }
     public void EnableCropButtonPressed()
     {
         //C'est la vie. Need a couple things here. First, we have to turn off the UI for entering the URL for the image. Then we'll need to turn on 
